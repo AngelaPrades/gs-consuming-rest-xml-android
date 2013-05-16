17f4279c042181c710e61c396405185530762ca8
Roy Clarkson <rclarkson@gopivotal.com>
2013-05-16 09:18:30 -0400

    Update example code

--- complete/src/main/java/org/hello/HelloActivity.java
+++ complete/src/main/java/org/hello/HelloActivity.java
@@ -21,6 +21,7 @@ import org.springframework.web.client.RestTemplate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloActivity extends Activity {
 
@@ -32,12 +33,16 @@ public class HelloActivity extends Activity {
 
 	@Override
 	public void onStart() {
+		super.onStart();
 		final String url = "http://10.0.2.2:8080/hello-world";
 
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
 
 		Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+
+		TextView textView = (TextView) this.findViewById(R.id.text_view);
+		textView.setText(greeting.getContent());
 	}
 
 }
