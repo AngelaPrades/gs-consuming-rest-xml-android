f6fa06a6c8259d14b4af9cb6ca12797c32a747e8
Roy Clarkson <rclarkson@gopivotal.com>
2013-06-21 16:56:10 -0400

    Rename layout file to make it more self explanatory

--- complete/src/main/java/org/hello/HelloActivity.java
+++ complete/src/main/java/org/hello/HelloActivity.java
@@ -28,7 +28,7 @@ public class HelloActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.hello_layout);
     }
 
     @Override
